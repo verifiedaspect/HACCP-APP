17dfb1820d83e6a84e47192a4ba3a3958fd33c2b
Naud <verifiedaspect@gmail.com>
2025-12-14 12:33:19 +0100

    Poort aangepast voor Render

--- server.js
+++ server.js
@@ -460,4 +460,6 @@ app.get("/checklist/list", (req, res) => {
 // ==========================
 // SERVER START
 // ==========================
-app.listen(3000, () => console.log("HACCP webapp draait op http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`HACCP webapp draait op http://localhost:${PORT}`));
+
